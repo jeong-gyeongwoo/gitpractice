72e4981863e5faf62d2b9eb89c37254d0ad75f5e
gyeong <okbamtol@naver.com>
2023-07-25 12:35:58 +0900

    commit뷰컨 추가

---
+++ gitpractice/CommitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommitViewController.swift
+//  gitpractice
+//
+//  Created by 정경우 on 2023/07/25.
+//
+
+import UIKit
+
+class CommitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
